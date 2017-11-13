f20c537a806ea30ebf0aabcb49c0427237cc0b8a
Nguyên Phương Nguyễn <nguyenphuongnguyenpn@gmail.com>
2017-11-13 18:47:24 +0700

    Update index_page

---
+++ js/gam-comic.js
@@ -0,0 +1,8 @@
+$('.SeeMore2').click(function(){
+    $(this).toggleClass('SeeMore2');
+    if($(this).hasClass('SeeMore2')){
+        $(this).text('See More');         
+    } else {
+        $(this).text('See Less');
+    }
+});
